b47cb3af8db872eae71b546cdc82a3bfcb45696b
Myke Kolawole <105569489+Mykol5@users.noreply.github.com>
2026-03-27 19:59:53 +0100

    Create main.dart

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:universal_platform/universal_platform.dart';
+import 'providers/stream_provider.dart';
+import 'mobile/screens/streamer_setup_screen.dart';
+import 'web/screens/web_viewer_screen.dart';
+
+void main() {
+  runApp(MultiCastProApp());
+}
+
+class MultiCastProApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => StreamProvider(),
+      child: MaterialApp(
+        title: 'MultiCast Pro',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          useMaterial3: true,
+        ),
+        home: UniversalPlatform.isWeb 
+            ? WebViewerScreen()
+            : StreamerSetupScreen(),
+      ),
+    );
+  }
+}
